da79fa55b88c1de472ab5df88efe50ab8468b233
LiquidFox <ginyz@foxmail.com>
2021-03-23 12:55:23 +0800

    kafka更新

--- src/main/java/com/cjs/flumeKafkaLearn/kafkaLearn/producerLearn/MyPartitions.java
+++ src/main/java/com/cjs/flumeKafkaLearn/kafkaLearn/producerLearn/MyPartitions.java
@@ -4,12 +4,30 @@ import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class MyPartitions implements Partitioner {
+    /**
+     *
+     * @param s 主题名
+     * @param o　The key to partition on (or null if no key)
+     * @param bytes　serialized key to partition on (or null if no key)
+     * @param o1　The value to partition on or null
+     * @param bytes1　serialized value to partition on or null
+     * @param cluster　The current cluster metadata
+     * @return
+     */
     @Override
     public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+
+        System.out.println(s);
+        System.out.println(o);
+        System.out.println(new String(bytes, StandardCharsets.UTF_8));
+        System.out.println(o1);
+        System.out.println(new String(bytes1,StandardCharsets.UTF_8));
 
         List<PartitionInfo> partitionInfos = cluster.availablePartitionsForTopic(s);
 
